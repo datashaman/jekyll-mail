e2c6e2c524b08b8510510cb697d0301fa265d10e
datashaman <marlinf@datashaman.com>
2018-12-05 09:02:44 +0200

    wip

--- app.rb
+++ app.rb
@@ -12,9 +12,21 @@ if ARGV.length != 1
     abort('Please specify the Jekyll site folder as the first parameter')
 end
 
-site = ARGV[0]
+site = ARGV[0].chomp
 
 content = STDIN.read
 mail = Mail.read_from_string(content)
 
-$LOG.debug("Site: #{site}, Mail: #{mail}")
+title_slug = mail.subject.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+posts_folder = "#{site}/_posts"
+post_file = "#{posts_folder}/#{mail.date.to_date.to_s}-#{title_slug}.md"
+
+$LOG.debug("Site: #{site}, Mail: #{mail}, Post file: #{post_file}")
+
+File.open(post_file, 'w', 0664) { |f|
+    f.write("---\n")
+    f.write("layout: post\n")
+    f.write("title: #{mail.subject}\n")
+    f.write("---\n")
+    f.write(mail.decoded)
+}
